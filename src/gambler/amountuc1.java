61be69a22245d4a3d6d79acbbcb5408f0fd985c8
unknown <SmithaRai.com>
2022-04-04 09:08:36 +0530

    first commit

---
+++ src/gambler/amountuc1.java
@@ -0,0 +1,11 @@
+package gambler;
+
+public class amountuc1 {
+	static int amount = 100;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("amount the gambler has:"+amount);
+	}
+
+}
