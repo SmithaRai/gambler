84a672d68ff4fadab231ab65207acd11a779a5b2
unknown <SmithaRai.com>
2022-04-04 09:15:04 +0530

    second commit

---
+++ src/gambler/BetUc2.java
@@ -0,0 +1,15 @@
+package gambler;
+
+public class BetUc2 {
+static int amount = 100;
+static int bets = 1;
+static int won =0;
+static int loss=0;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("bets:"+bets);
+	}
+
+}
